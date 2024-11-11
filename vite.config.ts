b444138f2bb056baaf9233bd2bd8c3041a9dc554
Valentin Thomas <me@valentin-thomas.com>
2024-11-10 17:03:28 -0800

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,13 @@ import path from "node:path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
 
-const _dirname = typeof __dirname !== 'undefined'
-  ? __dirname
-  : dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "~": path.resolve(_dirname, "./src"),
+      "~": path.resolve(__dirname, "./src"),
     },
   },
 })
